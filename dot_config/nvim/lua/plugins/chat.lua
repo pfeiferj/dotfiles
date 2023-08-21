73d7f15de5417fbbbe44e44543937dce914b1b11
Jacob Pfeifer <jacob@pfeifer.dev>
2023-08-21 01:36:09 -0400

    more tokens

--- dot_config/nvim/lua/plugins/chat.lua
+++ dot_config/nvim/lua/plugins/chat.lua
@@ -2,6 +2,12 @@ return {
   'jackMort/ChatGPT.nvim',
   cmd = {'ChatGPT', 'ChatGPTActAs', 'ChatGPTCompleteCode', 'ChatGPTEditWithInstructions', 'ChatGPTRun'},
   opts = {
-    api_key_cmd = vim.fn.expand('$HOME') .. '/.config/.openai'
+    api_key_cmd = vim.fn.expand('$HOME') .. '/.config/.openai',
+    popup_input = {
+      max_visible_lines = 100
+    },
+    openai_params = {
+      max_tokens = 2000,
+    },
   }
 }
